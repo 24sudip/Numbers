13bdbdf7e8efc0b319b771eadb930f6657038b27
unknown <sudip2tenber@gmail.com>
2024-02-13 16:20:25 +0600

    Numbers

---
+++ app.js
@@ -0,0 +1,29 @@
+// http://numbersapi.com/
+
+const factBtn = document.querySelector("button");
+const factInput = document.querySelector("input");
+
+const factPara = document.querySelector("p");
+const factSelect = document.querySelector("select");
+
+factBtn.addEventListener("click", () => {
+    const number = factInput.value;
+    const factType = factSelect.value || "trivia";
+
+    if (Number.isFinite(parseInt(number))) {
+        const numberFact = parseInt(number);
+        fetchFact(numberFact, factType);
+    } else if(number === "random"){
+        fetchFact(number, factType);
+    } else {
+        return;
+    }
+});
+
+async function fetchFact(number, type) {
+    const factURL = "http://numbersapi.com/"; 
+
+    const response = await fetch(`${factURL}${number}/${type}`);
+    const responseData = await response.text();
+    factPara.innerText = responseData;
+}
